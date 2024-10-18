9a707d398e15ae6d8db635f5e34f60e334b9d84c
katelyn-zhao <146498901+katelyn-zhao@users.noreply.github.com>
2024-10-18 14:15:42 -0700

    Update rigid_img_reg.m

--- rigid_img_reg.m
+++ rigid_img_reg.m
@@ -1,17 +1,21 @@
 % Load the fixed and moving images (NIfTI format)
-fixedImage = double(niftiread("23-TR-0002/PreTreatment Imaging/precontrast_23-TR-0002_20110731_900 tumor sphere seg.nii"));
+fixedImage = double(niftiread("precontrast_23-TR-0002_20110731_900.nii"));
 
-movingImage = double(niftiread("23-TR-0002/PreTreatment Imaging/delayed_23-TR-0002_20110731_12 tumor sphere seg.nii"));
+movingImage = double(niftiread("portalvenous_23-TR-0002_20110731_10.nii"));
 
 fixedImage = fixedImage / max(fixedImage(:));
 movingImage = movingImage / max(movingImage(:));
 
 % Set up optimizer and metric
 [optimizer, metric] = imregconfig('monomodal');
+optimizer.MaximumStepLength = 0.001;
 
 % Perform registration
 tform = imregtform(movingImage, fixedImage, 'rigid', optimizer, metric);
 registered = imwarp(movingImage, tform, 'OutputView', imref3d(size(fixedImage)));
+
+%Save the transformation object
+save('portalvenous_affine.mat', 'tform');
 
 % Compute metrics
 nmi_value = computeNMI(fixedImage, registered);
@@ -25,7 +29,16 @@ fprintf('NMI: %.4f\n', nmi_value);
 fprintf('ECC: %.4f\n', ecc_value);
 fprintf('ICE: %.4f\n', ice_value);
 
-% Visualize
+% Visualize Overlayed
 figure;
 imshowpair(squeeze(fixedImage(:,:,end/2)), squeeze(registered(:,:,end/2)));
 title('Fixed vs Registered Image');
+
+% Visualize Side by Side
+figure;
+subplot(1, 2, 1);
+imshow(squeeze(fixedImage(:,:,end/2)), []);
+title('Fixed Image');
+subplot(1, 2, 2);
+imshow(squeeze(registered(:,:,end/2)), []);
+title('Registered Image');
